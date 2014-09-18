421c79a03139f6865d809ef5ecebb0102b42f27a
aoede3 <john@aoede.uk.net>
2014-09-18 09:21:03 +0000

    commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('templatesApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+    .controller("MainCtrl", function($scope, $http) {
+      $http.get('fraser.json').
+        success(function(data, status, headers, config) {
+          
+          $scope.items = data;
+        }).
+        error(function(data, status, headers, config) {
+          console.log("Data:" + data);
+        });
+    });
